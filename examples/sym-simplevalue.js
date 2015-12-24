4ce0fbc9f3c506033560c9d7ea57a6f4fe247e07
jgolla <jason@golla.me>
2015-12-24 10:14:08 -0500

    Adding simplevalue examples

---
+++ examples/sym-simplevalue.js
@@ -0,0 +1,39 @@
+(function (CS) {
+	var defintion = {
+	    typeName: 'simplevalue',
+	    datasourceBehavior: CS.DatasourceBehaviors.Single,
+		getDefaultConfig: function() {
+		    return {
+		        DataShape: 'Value',
+		        Height: 150,
+		        Width: 150,
+		        BackgroundColor: 'rgb(255,0,0)',
+		        TextColor: 'rgb(0,255,0)',
+		        ShowLabel: true,
+		        ShowTime: false
+		    };
+		},
+	    configOptions: function (symbol) {
+	        return [{
+	            title: 'Format Symbol',
+	            mode: 'format'
+	        }];
+	    },
+	    init: init
+	};
+
+	function init(scope) {
+	    function onUpdate(data) {
+	        if(data) {
+	            scope.value = data.Value;
+	            scope.time = data.Time;
+	            if(data.Label) {
+	                scope.label = data.Label;
+	            }
+	        }
+	    }
+	    return { dataUpdate: onUpdate };
+	}	
+
+    CS.symbolCatalog.register(defintion);
+})(window.Coresight);
